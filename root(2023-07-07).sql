0617b6d01afe8c5cb1f5a5ac476a8363b999e78e
humi53 <j.k.humi9@gmail.com>
2023-07-07 16:41:12 +0900

    쿼리문들참조키2023-07-07

---
+++ root(2023-07-07).sql
@@ -0,0 +1,24 @@
+-- 여기는 root 화면
+-- 유저 삭제
+DROP USER 'todo'@'localhost';
+DROP USER 'todo'@'192.168.4.%';
+DROP USER 'todo'@'%';
+
+-- 사용자 생성
+DROP USER todo@localhost;
+CREATE USER todo@localhost IDENTIFIED BY '88888888';
+-- 권한부여
+GRANT ALL PRIVILEGES ON todoDB.* TO todo@localhost;
+-- 권한적용
+FLUSH PRIVILEGES;
+
+-- MySQL 의 SCHEMA 생성
+CREATE DATABASE scoreDB;
+GRANT ALL PRIVILEGES ON scoreDB.* TO todo@localhost;
+FLUSH PRIVILEGES;
+
+
+
+
+
+
